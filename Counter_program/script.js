01f5919a8f04a490225df723cfe3754409f630f6
Sneha Guttedar <snehaguttedarzg@gmail.com>
2026-01-05 00:42:49 +0530

    done

--- Counter_program/script.js
+++ Counter_program/script.js
@@ -1,15 +1,22 @@
-// const container=document.getElementsByClassName("container");
-const l1=document.getElementById("l1");
-const b1=document.getElementById("b1");
-const b2=document.getElementById("b2");
-const b3=document.getElementById("b3");
-let count=0;
+const l1 = document.getElementById("l1");
+const b1 = document.getElementById("b1");
+const b2 = document.getElementById("b2");
+const b3 = document.getElementById("b3");
 
-// b1.onclick=function(){
-//     count++;
-//     l1.textContent=count;
-// }
-b1.addEventListener("click", function() {
+let count = 0;
+
+b1.onclick = function () {
     count++;
     l1.textContent = count;
-});
+};
+
+b2.onclick = function () {
+    count--;
+    l1.textContent = count;
+};
+
+b3.onclick = function () {
+    count = 0;
+    l1.textContent = count;
+};
+
